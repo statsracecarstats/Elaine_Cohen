4c7c903b2be545187d49b12c1c38bb22590c3cf7
Paul Cunningham <pacunningham821@gmail.com>
2019-06-27 22:36:49 -0400

    GD rotate

--- js/E.js
+++ js/E.js
@@ -8,9 +8,27 @@ var canvas = d3.select(".art").append("svg")
 				.attr("width", width + margin.left + margin.right)
 				.attr("height", height + margin.top + margin.bottom);
 
+var x = 100;
+var y = 200;
+
 var bar1 = canvas.append("rect")
-  .attr("x", 40)
-  .attr("y", 200)
+  .attr("x", x)
+  .attr("y", y)
   .attr("width", 100)
   .attr("fill", d3.rgb(170, 90, 230))
   .attr("class", "colorbar");
+
+var bar2 = canvas.append("rect")
+  .attr("x", x+100)
+  .attr("y", y+100)
+  .attr("width", 100)
+  .attr("fill", d3.rgb(40, 190, 230))
+  .attr("class", "colorbar");
+
+var bar3 = canvas.append("rect")
+  .attr("x", x)
+  .attr("y", y+100)
+  .attr("width", 100)
+  .attr("fill", d3.rgb(240, 190, 40))
+  .attr("class", "colorbar")
+  .attr("transform", "rotate (-90)");
